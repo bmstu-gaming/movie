4e0a85540f8a7cff8c3998b364579f8b30a1b12e
Ilgiz <38355923+EkT1oN@users.noreply.github.com>
2024-01-22 18:38:31 +0300

    Small fix
    - change double quotas to single
    - add leading space for LOG info & error

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from movie.runner import run
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run()
--- movie/utils/constants.py
+++ movie/utils/constants.py
@@ -1,6 +1,6 @@
-CHECK = "\u2714"  # ✔ check mark
-CROSS = "\u274c"  # ❌ cross mark
-MKV_STREAMS = r"Stream #[0-9]*:([0-9]*)(\([a-z]*\))?: ([a-z,A-Z]*)"
-MP4_STREAMS = r"Stream #[0-9]*:([0-9]*)\[[0-9]x[0-9]\](\([a-z]*\))?: ([a-z,A-Z]*)"
-TITLE_STREAMS = r"Stream #[0-9]*:([0-9]*)(\([a-z]*\)): ([a-z,A-Z]*):(?<=[:]).*(?=[\n])\s+Metadata:\n\s+title\s+: (.*(?=[\n]))"
-SUBTITLE_TYPE = "Subtitle"
+CHECK = '\u2714'  # ✔ check mark
+CROSS = '\u274c'  # ❌ cross mark
+MKV_STREAMS = r'Stream #[0-9]*:([0-9]*)(\([a-z]*\))?: ([a-z,A-Z]*)'
+MP4_STREAMS = r'Stream #[0-9]*:([0-9]*)\[[0-9]x[0-9]\](\([a-z]*\))?: ([a-z,A-Z]*)'
+TITLE_STREAMS = r'Stream #[0-9]*:([0-9]*)(\([a-z]*\)): ([a-z,A-Z]*):(?<=[:]).*(?=[\n])\s+Metadata:\n\s+title\s+: (.*(?=[\n]))'
+SUBTITLE_TYPE = 'Subtitle'
